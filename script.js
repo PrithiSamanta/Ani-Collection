15c926e0f4ceb5e6640840397590ec7e18bb11ae
Prithi Samanta <samantaprithi@gmail.com>
2026-05-22 14:54:33 +0530

    Add responsiveness

--- script.js
+++ script.js
@@ -8,55 +8,6 @@ searchIcon.addEventListener("click", (e) => { //focuses the input when the icon 
 })
 
 
-async function getTopAiringAnime() {//top airing anime
-    const url = "https://kitsu.io/api/edge/anime?filter[status]=current&sort=-userCount&page[limit]=5";
-
-    try {
-        let response = await fetch(url);
-
-        let result = await response.json();
-
-        console.log(result);
-
-        let carouselItems = document.querySelectorAll(".carousel-item");
-
-
-
-        let animeList = result.data;
-        // mainSection.style.backgroundImage = `url(${result.data[0].images.jpg.large_image_url})`;
-
-
-        // animeList.forEach(element => {
-
-        //     let animeImg = element.images.jpg.image_url;
-
-
-        //     carouselItems.forEach(item => {
-        //         const img = item.querySelector('img');
-
-        //         img.src = animeImg;
-        //     }
-
-        //     );
-        // });
-
-        carouselItems.innerHTML = "";
-        for (let i = 0; i < animeList.length; i++) {
-
-            let animeImg = animeList[i].attributes.coverImage.original;
-
-            carouselItems[i].innerHTML = `<img src=${animeImg}>`
-
-        }
-
-
-
-    }
-    catch (err) {
-        console.log("Error occured ", err)
-    }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     getCurrentlyAiringAniList();
 });
@@ -131,14 +82,14 @@ async function getCurrentlyAiringAniList() {
                     <img src="${banner}" class="d-block w-100 carousel-banner-img" alt="${title} Banner">
                     <div class="carousel-caption d-flex flex-column justify-content-center align-items-start text-start px-4 py-3 rounded">
                         
-                        <h2 class="fw-bold text-truncate display-3 mb-5" style="max-width: 60%;">${title}</h2>
+                        <h2 class="fw-bold text-truncate display-4 mb-5" style="max-width: 60%;">${title}</h2>
                         <div class=" status-div ">
                                 <span class="badge badge-meta me-2 bg-dark text-warning border border-secondary border-opacity-50">★ ${score}</span>
                                 <span class="badge badge-meta me-2 bg-dark text-light border border-secondary border-opacity-50"><i class="bi bi-play-btn-fill me-1"></i>${totalEpisodes}</span>
                               <span class="badge badge-meta me-2 bg-dark text-info border border-secondary border-opacity-50">${releaseStatus}</span>
                             </div>
                          <p class="line-clamp" style="max-width: 50%;">${cleanDescription}</p>
-                        <button class="btn text-white btn-lg fs-4 fw-bold"><i class="bi bi-info-circle pe-2"></i>Details</button>
+                        <button class="btn text-white btn-lg fs-4 fw-bold"><i class="bi bi-info-circle pe-2"></i>View Details</button>
                     </div>
                     </div>
                 `;
